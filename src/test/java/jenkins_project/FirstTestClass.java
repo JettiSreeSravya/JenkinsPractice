d20e9681277a4e72206a1401b6406c7c5641c36f
Sravya <sravya.jetti@amzur.com>
2026-06-18 14:31:50 +0530

    Added Browser close method

--- src/test/java/jenkins_project/FirstTestClass.java
+++ src/test/java/jenkins_project/FirstTestClass.java
@@ -2,15 +2,21 @@ package jenkins_project;
 
 import java.io.IOException;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import utilities.GenericMethods;
 
-public class FirstTestClass extends GenericMethods{
-	
+public class FirstTestClass extends GenericMethods {
+
 	@Test
 	public void test() throws IOException {
 		openUrl();
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
 	}
 
 }
